259338ad009fc77d288c9e9f480a02babdceacb7
v-v-d <v-v-d@bk.ru>
2019-11-02 00:34:43 +0300

    task 1 - tasks, index.html and script.js are added

---
+++ lesson_1/script.js
@@ -0,0 +1,28 @@
+// 1. Добавьте стили для верхнего меню, товара, списка товаров и кнопки вызова корзины.
+// 2. Добавьте значения по умолчанию для аргументов функции. Как можно упростить или
+// сократить запись функций?
+// 3. * Сейчас после каждого товара на странице выводится запятая. Из-за чего это происходит?
+// Как это исправить?
+
+'use strict';
+
+window.addEventListener('load', () => {
+  const goods = [
+    {title: 'Shirt', price: 150},
+    {title: 'Socks', price: 50},
+    {title: 'Jacket', price: 350},
+    {title: 'Shoes', price: 250},
+  ];
+
+  const renderGoodsItem = (title, price) => {
+    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+  };
+
+  const renderGoodsList = (list) => {
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+
+    document.querySelector('.goods-list').innerHTML = goodsList;
+  };
+
+  renderGoodsList(goods);
+});
