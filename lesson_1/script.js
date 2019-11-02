c7f3f6bfdaa2e34c455f1651f194c4f3b0be6314
v-v-d <v-v-d@bk.ru>
2019-11-02 03:54:51 +0300

    task 2 done

--- lesson_1/script.js
+++ lesson_1/script.js
@@ -1,6 +1,8 @@
 // 1. Добавьте стили для верхнего меню, товара, списка товаров и кнопки вызова корзины.
 // 2. Добавьте значения по умолчанию для аргументов функции. Как можно упростить или
 // сократить запись функций?
+// Ответ: из функции renderGoodsItem убрать return и {}, в функции renderGoodsList в .innerHTML передеать сразу результат
+// выполнения list.map(...).join('')
 // 3. * Сейчас после каждого товара на странице выводится запятая. Из-за чего это происходит?
 // Как это исправить?
 // Ответ: метод .map() возвращает массив, далее innerHTML вставляет массив в разметку, где он разворачивается
@@ -16,9 +18,7 @@ window.addEventListener('load', () => {
     {title: 'Shoes', price: 250},
   ];
 
-  const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-  };
+  const renderGoodsItem = (title, price, text = '') => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 
   const renderGoodsList = list => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
