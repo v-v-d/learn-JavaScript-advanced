014ae852c8b2aad04272b68f3d0a3683f34d7ab5
v-v-d <v-v-d@bk.ru>
2019-11-19 22:19:10 +0300

    all packs for project are collected in install_it.bat

---
+++ final/install_it.bat
@@ -0,0 +1,4 @@
+npm i -D webpack webpack-cli @babel/core @babel/plugin-syntax-dynamic-import @babel/preset-env babel-loader css-loader html-webpack-plugin node-sass sass-loader vue-loader vue-style-loader vue-template-compiler webpack-dev-server jasmine
+pause
+npm i vue vue-router
+pause
