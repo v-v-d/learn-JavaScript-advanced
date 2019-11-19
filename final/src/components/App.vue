57089333f5f2ac7b78cf45cba0649b2071c7be95
v-v-d <v-v-d@bk.ru>
2019-11-19 23:15:21 +0300

    app.vue added, smoke test done

---
+++ final/src/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">hello from vue</div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang="scss">
+  .app {
+    background-color: aqua;
+  }
+</style>
