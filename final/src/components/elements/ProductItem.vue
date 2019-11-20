e4753fb2bc24f5dd9cb841bb176542156e0bf806
v-v-d <v-v-d@bk.ru>
2019-11-20 03:28:01 +0300

    ProductItem.vue added to vue components

---
+++ final/src/components/elements/ProductItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <img :src="img" class="card-product-img" alt="img">
+    <div class="card-body">
+      <h5 class="card-title">{{ name }}</h5>
+      <p class="card-text">price: {{ price }}</p>
+      <a href="#" @click="buyButtonHandler(id)" class="btn btn-primary buy-btn">Add&nbsp;to&nbsp;cart</a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProductItem',
+    props: ['id', 'name', 'price', 'img'],
+    methods: {
+      buyButtonHandler(id) {
+        this.$emit('buy', id);
+      },
+    },
+  }
+</script>
+
+<style lang="scss">
+  .card-product-img {
+    width: 300px;
+    height: 300px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    align-self: center;
+  }
+</style>
