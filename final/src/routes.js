fb027757bb62fbecb095cb31d82eb96e3aae16c5
v-v-d <v-v-d@bk.ru>
2019-11-20 04:09:00 +0300

    routes.js updated but still needs to fix routes

--- final/src/routes.js
+++ final/src/routes.js
@@ -1,23 +1,44 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-const HomePage = () => import(/* webpack-chunk-name: "HomePage" */ './components/pages/HomePage.vue');
-const CatalogPage = () => import(/* webpack-chunk-name: "CatalogPage" */ './components/pages/CatalogPage.vue');
+const ProductsPage = () => import(/* webpack-chunk-name: "ProductsPage" */ './components/pages/ProductsPage.vue');
+const CartPage = () => import(/* webpack-chunk-name: "CartPage" */ './components/pages/CartPage.vue');
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
   routes: [
     {
-      name: 'home',
+      name: 'products',
       path: '/',
-      component: HomePage,
+      component: ProductsPage,
     },
     {
-      name: 'catalog',
-      path: '/catalog',
-      component: CatalogPage,
+      name: 'cart',
+      path: '/cart',
+      component: CartPage,
     },
   ],
   mode: 'history',
 });
+
+// const HomePage = () => import(/* webpack-chunk-name: "HomePage" */ './components/pages/HomePage.vue');
+// const CatalogPage = () => import(/* webpack-chunk-name: "CatalogPage" */ './components/pages/CatalogPage.vue');
+//
+// Vue.use(VueRouter);
+//
+// export default new VueRouter({
+//   routes: [
+//     {
+//       name: 'home',
+//       path: '/',
+//       component: HomePage,
+//     },
+//     {
+//       name: 'catalog',
+//       path: '/catalog',
+//       component: CatalogPage,
+//     },
+//   ],
+//   mode: 'history',
+// });
