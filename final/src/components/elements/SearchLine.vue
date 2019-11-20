085ff22f574996f873cc4da9b1aef59a3dd55cc0
v-v-d <v-v-d@bk.ru>
2019-11-20 03:50:56 +0300

    SearchLine.vue added to vue components

---
+++ final/src/components/elements/SearchLine.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search-line">
+    <input v-model="query" type="text" class="query" placeholder="search"/>
+    <div @click="searchButtonHandler" class="btn btn-secondary search-button">Find</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SearchLine',
+    data() {
+      return {
+        query: '',
+      }
+    },
+    methods: {
+      searchButtonHandler() {
+        this.$emit('search', this.query);
+      },
+    },
+  }
+</script>
+
+<style lang="scss">
+  .search-button {
+    margin-right: 40px;
+    margin-bottom: 3.7px;
+  }
+</style>
